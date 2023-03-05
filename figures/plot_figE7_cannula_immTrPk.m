f0dca286292ad8c09e1d40107eeb5d9c51181651
ackrok <51799203+ackrok@users.noreply.github.com>
2023-03-05 15:24:01 -0500

    Update plot_figE7_cannula_immTrPk.m

--- figures/plot_figE7_cannula_immTrPk.m
+++ figures/plot_figE7_cannula_immTrPk.m
@@ -21,8 +21,7 @@ for z = 1:2 % plot troughs and peaks
     for y = 1:length(plotme)
         mat = plotme{y};
         mat = abs(mat);
-        mat(isnan(mat)) = 0;
-        mat(4,:) = nan;
+        % mat(isnan(mat)) = 0;
         % mat = mat./nanmean(mat(:,1));
         [~,p] = ttest(mat(:,1),mat(:,2));
         
